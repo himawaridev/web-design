f81fc94031d0eeac074fb51a50b4f039cad79b4a
dunghoang <dunghoang22102003@gmail.com>
2023-09-28 12:47:44 +0700

    update

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,5 @@
 const connection = require("../config/database");
-const { getAllUsers } = require('../service/CRUDService');
+const { getAllUsers, getUserById, updateUserById, deleteUserById } = require('../service/CRUDService');
 
 
 const getHomePage = async (req, res) => {
@@ -20,9 +20,58 @@ const getCreatePage = (req, res) => {
   res.render("create.ejs");
 }
 
+const getUpdatePage = async (req, res) => {
+  const userId = req.params.id;
+  let user = await getUserById(userId);
+  res.render("edit.ejs", {userEdit : user});
+}
+
+
+//---------------------------------------------
 const postCreateUser = async (req, res) => {
   let {email, name, city} = req.body;
   console.log('>>> req.email = ', email, '>>> req.name = ', name, '>>> req.city = ', city);
+  let [results, fields] = await connection.query(
+    `INSERT INTO Users (email, name, city)
+    VALUES (?, ?, ?);`, [email, name, city]
+    );
+    console.log('>>> req.email = ', results);
+    res.send('create users success');
+}
+
+const postUpdateUser = async (req, res) => {
+  let {email, name, city, userId} = req.body;
+  // console.log('>>> req.email = ', email, '>>> req.name = ', name, '>>> req.city = ', city, '>>> req.userId = ', userId);
+  //   console.log('>>> req.email = ', results);
+  await updateUserById(email, city, name, userId);
+    // res.send('Updated users succeed');
+    res.redirect('/');
+}
+
+const postDeleteUser = async (req, res) => {
+  const userId = req.params.id;
+  let user = await getUserById(userId);
+  res.render('delete.ejs', {userEdit : user});
+}
+
+const postHandleRemoveUser = async(req, res) => {
+  const id = req.body.userId;
+  await deleteUserById(id);
+    res.redirect('/');
+}
+
+module.exports = {
+  getHomePage,
+  getDev, 
+  getHimawari,
+  getCreatePage,
+  postCreateUser,
+  getUpdatePage,
+  postUpdateUser,
+  postDeleteUser,
+  postHandleRemoveUser
+};
+
   // connection.query(
   //   `INSERT INTO Users (email, name, city)
   //   VALUES (?, ?, ?);`,
@@ -32,25 +81,7 @@ const postCreateUser = async (req, res) => {
   //     res.send('create users success');
   //   }
   // );
-  let [results, fields] = await connection.query(
-    `INSERT INTO Users (email, name, city)
-    VALUES (?, ?, ?);`, [email, name, city]
-    );
-    console.log('>>> req.email = ', results);
-    res.send('create users success');
-
   // connection.query("select * from Users u;", function (err, results, fields) {
   //   console.log(">>> results = ", results); // results contains rows returned by server
   // });
   // const [results, fields] = await connection.query("select * from Users u;");
-
-
-}
-
-module.exports = {
-  getHomePage,
-  getDev,
-  getHimawari,
-  getCreatePage,
-  postCreateUser,
-};
--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,10 @@ const {
     getHimawari,
     getCreatePage,
     postCreateUser,
+    getUpdatePage,
+    postUpdateUser,
+    postDeleteUser,
+    postHandleRemoveUser
 } = require("../controller/homeController");
 const router = express.Router();
 
@@ -14,6 +18,10 @@ router.get("/dev", getDev);
 router.get("/himawari", getHimawari);
 router.get("/create", getCreatePage);
 router.post("/create-user", postCreateUser);
+router.get("/update/:id", getUpdatePage);
+router.post("/update-user", postUpdateUser);
+router.post("/delete-user/:id", postDeleteUser);
+router.post("/delete-user", postHandleRemoveUser);
 
 
 module.exports = router;
